eb8084d037166fd0149a97e950f2e22c3a941261
MaelleDaniel <maelledaniel2@gmail.com>
2021-09-01 11:18:34 +0200

    let's start

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Scotland style</h1>
+      <p>PAttin couffin...blblblb</p>
+    </div>
+  );
+}
+
+export default App;
